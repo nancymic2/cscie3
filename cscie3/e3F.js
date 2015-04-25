5b1e914efa7b177fc6b22a80d44227f7dcd509eb
nancy mitchell <nancy.mitchell@hp.com>
2015-04-25 13:31:39 -0400

    first commit

--- cscie3/e3F.js
+++ cscie3/e3F.js
@@ -4,8 +4,20 @@ for (var i=0; i<tds.length; i++) {
     tds[i].addEventListener("click", countCard);
   
   //create array of clicked items ids
+  //data-vals represent card value
+  //innterText will be face of card
+  //will have to compare data-vals vs face 
+  //will have to set dyn the data-vls
    
 }
+
+tds[1].setAttribute("data-vals", "232323");
+var test=tds[1].getAttribute("data-vals"); //put card
+//value into custm attrib
+//then get
+//then test
+alert(test);
+
 //var turns= new Array();
 var turns= [];
 
@@ -24,26 +36,70 @@ function showCard(myid, which) {
   counter ++;
  
   //alert(this.id); //the id at the time of caller
-
+  //alert (counter);
+  //alert(which);
   prev=caught;
-
+  //prevID=
   caught=document.getElementById(myid.id).innerText;
-
+     //alert(prev);
+     // alert (caught);
   if (prev==caught){
     alert("score!");
-   
+          //alert(counter);
+          //alert(which);
+            //x=turns[counter];
+            //alert(myid.id);
  
-    document.getElementById(turns[counter-1]).innerText="turn"+counter;
-    document.getElementById(turns[counter]).innerText="turn"+ (counter+1);
-
+            //newer=(turns[(which.id)-1]);
+              //alert(newer);
+    var repl= document.getElementById(turns[counter-1]).getAttribute("data-vals");
+    alert(repl);
+    document.getElementById(turns[counter-1]).innerText=repl;
+    
+    var repl2= document.getElementById(turns[counter]).getAttribute("data-vals");
+    //document.getElementById(turns[counter-1]).innerText="turn"+counter;
+    //document.getElementById(turns[counter]).innerText="turn"+ (counter+1);
+    
+    document.getElementById(turns[counter]).innerText=repl2;
+    
+        //alert(turns[counter]);
+        //alert(turns[counter-1]);
   }
   else {
     
     
   }
 }
+   
 
 
+
+
+
+
+/*
+var counter=0;
+function flipcheck() {
+    counter ++;
+    if (parseInt(counter/2)===counter/2) {
+        flipit(this); //the id at the time of caller
+    }
+}
+    
+
+function flipit(theTd){
+ 
+  alert (theTd.id);  //0based index
+  alert ( parseInt(theTd.id)-1); //prev sq
+  var prev=( parseInt(theTd.id)-1);
+  
+  //minus 1 will need to be rand numb
+  
+  document.getElementById(theTd.id).innerText="bye";
+  document.getElementById(prev).innerText="hello";
+    
+}
+*/
 
 
 //css
@@ -56,6 +112,8 @@ td {
     height: 30px;
     width 30px;
     cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
 }
 
 
@@ -65,7 +123,6 @@ td {
 //html
 
 /*
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -75,17 +132,29 @@ td {
 <body>
 <table>
     <tr>
-        <td>222</td> <td>888</td> <td>666</td> <td>555</td> 
+        <td data-vals="99999">222</td> 
+        <td data-vals="45333">888</td> 
+        <td data-vals="44333">666</td> 
+        <td data-vals="231214">555</td> 
     </tr>
     <tr>
-                <td>333</td> <td>666</td> <td>7777</td> <td>999</td> 
-            </tr>
+        <td data-vals="94993">333</td> 
+        <td data-vals="22222222">666</td> 
+        <td data-vals="75646">7777</td> 
+        <td data-vals="5587439">999</td> 
+    </tr>
     <tr>
-        <td>555</td> <td>888</td> <td>333</td> <td>444</td> 
-            </tr>
+        <td data-vals="457403">555</td> 
+        <td data-vals="9884848484">888</td> 
+        <td data-vals="33948">333</td> 
+        <td data-vals="565656">444</td> 
+    </tr>
     <tr>
-         <td>444</td> <td>222</td> <td>7777</td> <td>999</td> 
-            </tr>
+         <td data-vals="33222">444</td> 
+         <td data-vals="64646">222</td> 
+         <td data-vals="282828">7777</td> 
+         <td data-vals="32334">999</td> 
+     </tr>
 </table>
 </body>
 </html>
