8cfac323ab2b197433276f7ea57f013605774296
nancy mitchell <nancy.mitchell@hp.com>
2015-04-30 12:48:56 -0400

    delayworks

--- cscie3/delay.js
+++ cscie3/delay.js
@@ -1,5 +1,46 @@
+//now push random ids into array
+//check click one by one
+//if the id clicked is not == to the id at that
+//place in the array, immed stop
 
+var newcount=0;
 var counter=0;
+var counter9=Math.floor(Math.random() * (7 - 3)) + 3;
+
+var runnit=setInterval(function() {
+var lit=Math.floor((Math.random() * 9) + 1); 
+    //maybe pass rnd and decrement it here
+counter++;
+    //alert(counter);
+blink (lit, counter);  //.delay(3000);;
+}, 900);
+
+function blink(i, counter){
+stop(i, counter);  
+return $("#" + "a" + i).delay(500).fadeOut(200).fadeIn(200);
+}
+
+function stop(i, counter){
+    ///increment counter here in loop?
+ //newcount++;   
+   // alert(counter);
+    if (counter>counter9){
+      clearInterval(runnit);
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+/*var counter=0;
 var runnit=setInterval(function() {
    var lit=Math.floor((Math.random() * 9) + 1); 
 counter++;
@@ -20,3 +61,4 @@ function stop(counter){
         
     }
 }
+*/
