15901306842c6e7b8c18fe41aac529da3fa252a9
nancy mitchell <nancy.mitchell@hp.com>
2015-04-30 10:51:16 -0400

    delayworks

--- cscie3/delay.js
+++ cscie3/delay.js
@@ -4,18 +4,18 @@ var runnit=setInterval(function() {
    var lit=Math.floor((Math.random() * 9) + 1); 
 counter++;
    blink (lit, counter);  //.delay(3000);;
-}, 1600);
+}, 1200);
 
 
 function blink(i, counter){
 
 stop(counter);  
 
-return $("#" + "a" + i).delay(1000).fadeOut(1000).fadeIn(1000);
+return $("#" + "a" + i).delay(500).fadeOut(200).fadeIn(200);
 }
 
 function stop(counter){
-    if (counter>4){
+    if (counter>6){
       clearInterval(runnit);
         
     }
